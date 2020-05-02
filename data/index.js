7b522437e8d9dc8798bee29b2ff9618d93286447
tirdeaalex <sanydge@gmail.com>
2020-05-02 11:59:04 +0300

    setup

---
+++ data/index.js
@@ -0,0 +1,130 @@
+export const defaultTheme = 'dark'; // light
+
+export const Title = 'Tirdea Alexandru - Junior Python Developer';
+
+export const Resume = {
+    introduction: `{ 
+        Introduction = () => { 
+            Namaste: { 
+                Name: 'Tirdea Alexandru', 
+                Designation: 'Junior Python Developer' 
+            } 
+        } 
+}`,
+
+    experience: `{
+        Experience = () => {
+            Python Developer
+            Freelance
+            Period : June 2019 - Present
+        }
+}`,
+
+    skills: `{
+        Skills = () => {
+            Markup: {
+                html: 'Hyper Text Markup Language',
+                version: 5
+            },
+            Styling: {
+                css: 'Cascading Style Sheets',
+                bootStrap: 'Bootstrap'
+            },
+            
+            BuildTools: {
+                REST API
+                Linux
+                Flask
+                Selenium
+                Jupyter
+                MySQL
+                Django
+                Docker
+                Scrapy Framework
+                Pandas
+            },
+            Others: {
+                Pycharm - 3.
+                VirtualBox
+                Anaconda
+            },
+            Tools: {
+                github: 'Github',
+                gitlab: 'Gitlab',
+            }
+        }
+}`,
+
+    projects: `{
+        Projects = () => {
+            Website: {
+                url: <a target='_blank' href='https://tirdeaalex.herokuapp.com/'>'https://tirdeaalex.herokuapp.com'</a>
+                source: <a target='_blank' href='https://github.com/sanydge/website'>'https://github.com/sanydge/website'</a>
+            },
+            Data Collector: {
+                url: <a target='_blank' href='https://dataacollector.herokuapp.com/'>'https://dataacollector.herokuapp.com/'</a>
+                source: <a target='_blank' href='https://github.com/sanydge/data_collector'>'https://github.com/sanydge/data_collector'</a>
+            },
+            Blockchain on Python: {
+                url: <a target='_blank' href='https://pychainblock.herokuapp.com/'>'https://pychainblock.herokuapp.com/'</a>,
+                demo: <a target='_blank' href='https://www.youtube.com/watch?v=6_W90VfJYBA'>'Youtube'</a>,
+                source: <a target='_blank' href='https://github.com/sanydge/python_blockchain'>'https://github.com/sanydge/python_blockchain'</a>,
+            },
+        }
+}`,
+
+    playground: `{
+        Playground = () => {   
+            Backend REST API with Python & Django: {
+                url: <a target='_blank' href='https://github.com/sanydge/rest_api'>'https://github.com/sanydge/rest_api'</a>
+            },    
+            Scraping geekbuying.com: {
+                url: <a target='_blank' href='https://github.com/sanydge/Web-Scraping-2'>'https://github.com/sanydge/Web-Scraping-2'</a>
+            },
+            Scraping using Scrapy Splash Selenium: {
+                url: <a target='_blank' href='https://github.com/sanydge/Web-Scraping'>'https://github.com/sanydge/Web-Scraping'</a>
+            },
+            Scraping imdb.com: {
+                url: <a target='_blank' href='https://github.com/sanydge/Web-Scraping-3'>'https://github.com/sanydge/Web-Scraping-3'</a>
+            },
+            A program that returns English-word definitions: {
+                url: <a target='_blank' href='https://github.com/sanydge/english_dictionary'>'https://github.com/sanydge/english_dictionary'</a>
+            },
+            Bookstore: {
+                url: <a target='_blank' href='https://github.com/sanydge/bookstore'>'https://github.com/sanydge/bookstore'</a>
+            },
+            Website Blocker: {
+                url: <a target='_blank' href='https://github.com/sanydge/website_blocker'>'https://github.com/sanydge/website_blocker'</a>
+            },
+            Web Maps: {
+                url: <a target='_blank' href='https://github.com/sanydge/webmaps'>'https://github.com/sanydge/webmaps'</a>
+            },
+            Motion-detector: {
+                url: <a target='_blank' href='https://github.com/sanydge/web_mot_detector'>'https://github.com/sanydge/web_mot_detector'</a>
+            },
+            Financial Graph: {
+                url: <a target='_blank' href='https://github.com/sanydge/financial_graph'>'https://github.com/sanydge/financial_graph'</a>
+            },
+            Geocoder Web Service: {
+                url: <a target='_blank' href='https://github.com/sanydge/geocoder_web_service'>'https://github.com/sanydge/geocoder_web_service'</a>
+            },
+        }
+}`,
+
+    contact: `{
+        Contact = () => {       
+            {
+                email: 'sanydge@gmail.com',
+                mobile: '0040754568729'
+            }
+        }
+}`
+};
+
+
+export const SocialLinks = {
+    // resume: '/resume.pdf',
+
+    linkedin: 'https://www.linkedin.com/in/alexandru-tirdea/',
+    github: 'https://github.com/sanydge'
+}
---
+++ src/components/forkme/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ForkMe = (props) => {
+    return ''
+}
+
+export default ForkMe;
