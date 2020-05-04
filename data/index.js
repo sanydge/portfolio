2fb81941a04942d10da260c2c1c7b3143c9c54c6
tirdeaalex <sanydge@gmail.com>
2020-05-04 14:57:18 +0300

    email

--- data/index.js
+++ data/index.js
@@ -75,7 +75,7 @@ export const Resume = {
 
     playground: `{
         Playground = () => {   
-            Backend Rest API with Python & Django: {
+            Backend REST API with Python & Django: {
                 url: <a target='_blank' href='https://github.com/sanydge/rest_api'>'https://github.com/sanydge/rest_api'</a>
             },    
             Scraping geekbuying.com: {
